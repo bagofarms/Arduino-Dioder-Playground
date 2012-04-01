c273ada55d0dac19a569a853bf33395226f2c833
Jacob Bates <jacob.bates@gmail.com>
2012-04-01 14:49:35 -0400

    incorporated crossfading

--- Arduino Projects/SingleChannelRGBTimeControlled/SingleChannelRGBTimeControlled.ino
+++ Arduino Projects/SingleChannelRGBTimeControlled/SingleChannelRGBTimeControlled.ino
@@ -9,38 +9,165 @@ one channel, since i want the color to be uniform across all of the lights.
 */
 
 /* Choose the first 3 PWM pins.  9,10,11 are the other 3 */
-const int channel1PinR = 3;
-const int channel1PinG = 5;
-const int channel1PinB = 6;
+const int redPin = 3;
+const int grnPin = 5;
+const int bluPin = 6;
+
+// Color arrays
+int black[3]  = { 0, 0, 0 };
+int white[3]  = { 255, 255, 255 };
+int red[3]    = { 255, 0, 0 };
+int green[3]  = { 0, 255, 0 };
+int blue[3]   = { 0, 0, 255 };
+int orange[3] = { 255, 101, 0 };
+// etc.
+
+// Set initial color
+int redVal = black[0];
+int grnVal = black[1]; 
+int bluVal = black[2];
+
+// Initialize color variables
+int prevR = redVal;
+int prevG = grnVal;
+int prevB = bluVal;
+
+int wait = 5;      // 10ms internal crossFade delay; increase for slower fades
+int hold = 1000;       // Optional hold when a color is complete, before the next crossFade
+int DEBUG = 0;      // DEBUG counter; if set to 1, will write values back via serial
+int loopCount = 60; // How often should DEBUG report?
+int repeat = 0;     // How many times should we loop before stopping? (0 for no stop)
+int j = 0;          // Loop counter for repeat
+
 
 void setup() {
   // set pins as outputs
-  pinMode(channel1PinR, OUTPUT); 
-  analogWrite(channel1PinR, 0);
-  pinMode(channel1PinG, OUTPUT); 
-  analogWrite(channel1PinG, 0);
-  pinMode(channel1PinB, OUTPUT); 
-  analogWrite(channel1PinB, 0);
+  pinMode(redPin, OUTPUT);   // sets the pins as output
+  pinMode(grnPin, OUTPUT);   
+  pinMode(bluPin, OUTPUT); 
+
+  if (DEBUG) {           // If we want to see values for debugging...
+    Serial.begin(9600);  // ...set up the serial ouput 
+  }
 }
 
 
 void loop () {
-  /*
-  analogWrite(channel1PinR, random(0,255));
-  analogWrite(channel1PinG, random(0,255));
-  analogWrite(channel1PinB, random(0,255));
-  delay(random(30,200));
-  */
-  analogWrite(channel1PinG, 0);
-  analogWrite(channel1PinB, 0);
-  
-  for(int brightness = 0; brightness <= 255; brightness = brightness++){
-    analogWrite(channel1PinR, brightness);
-    delay(5);
-  }
-  for(int brightness = 254; brightness >= 0; brightness--){
-    analogWrite(channel1PinR, brightness);
-    delay(5);
+  crossFade(green);
+  crossFade(red);
+  crossFade(blue);
+  crossFade(orange);
+
+  if (repeat) { // Do we loop a finite number of times?
+    j += 1;
+    if (j >= repeat) { // Are we there yet?
+      exit(j);         // If so, stop.
+    }
   }
 }
 
+/* BELOW THIS LINE IS THE MATH -- YOU SHOULDN'T NEED TO CHANGE THIS FOR THE BASICS
+* 
+* The program works like this:
+* Imagine a crossfade that moves the red LED from 0-10, 
+*   the green from 0-5, and the blue from 10 to 7, in
+*   ten steps.
+*   We'd want to count the 10 steps and increase or 
+*   decrease color values in evenly stepped increments.
+*   Imagine a + indicates raising a value by 1, and a -
+*   equals lowering it. Our 10 step fade would look like:
+* 
+*   1 2 3 4 5 6 7 8 9 10
+* R + + + + + + + + + +
+* G   +   +   +   +   +
+* B     -     -     -
+* 
+* The red rises from 0 to 10 in ten steps, the green from 
+* 0-5 in 5 steps, and the blue falls from 10 to 7 in three steps.
+* 
+* In the real program, the color percentages are converted to 
+* 0-255 values, and there are 1020 steps (255*4).
+* 
+* To figure out how big a step there should be between one up- or
+* down-tick of one of the LED values, we call calculateStep(), 
+* which calculates the absolute gap between the start and end values, 
+* and then divides that gap by 1020 to determine the size of the step  
+* between adjustments in the value.
+*/
+
+int calculateStep(int prevValue, int endValue) {
+  int step = endValue - prevValue; // What's the overall gap?
+  if (step) {                      // If its non-zero, 
+    step = 1020/step;              //   divide by 1020
+  } 
+  return step;
+}
+
+/* The next function is calculateVal. When the loop value, i,
+*  reaches the step size appropriate for one of the
+*  colors, it increases or decreases the value of that color by 1. 
+*  (R, G, and B are each calculated separately.)
+*/
+
+int calculateVal(int step, int val, int i) {
+
+  if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
+    if (step > 0) {              //   increment the value if step is positive...
+      val += 1;           
+    } 
+    else if (step < 0) {         //   ...or decrement it if step is negative
+      val -= 1;
+    } 
+  }
+  // Defensive driving: make sure val stays in the range 0-255
+  if (val > 255) {
+    val = 255;
+  } 
+  else if (val < 0) {
+    val = 0;
+  }
+  return val;
+}
+
+/* crossFade() converts the percentage colors to a 
+*  0-255 range, then loops 1020 times, checking to see if  
+*  the value needs to be updated each time, then writing
+*  the color values to the correct pins.
+*/
+
+void crossFade(int color[3]) {
+  int stepR = calculateStep(prevR, color[0]);
+  int stepG = calculateStep(prevG, color[1]); 
+  int stepB = calculateStep(prevB, color[2]);
+
+  for (int i = 0; i <= 1020; i++) {
+    redVal = calculateVal(stepR, redVal, i);
+    grnVal = calculateVal(stepG, grnVal, i);
+    bluVal = calculateVal(stepB, bluVal, i);
+
+    analogWrite(redPin, redVal);   // Write current values to LED pins
+    analogWrite(grnPin, grnVal);      
+    analogWrite(bluPin, bluVal); 
+
+    delay(wait); // Pause for 'wait' milliseconds before resuming the loop
+
+    if (DEBUG) { // If we want serial output, print it at the 
+      if (i == 0 or i % loopCount == 0) { // beginning, and every loopCount times
+        Serial.print("Loop/RGB: #");
+        Serial.print(i);
+        Serial.print(" | ");
+        Serial.print(redVal);
+        Serial.print(" / ");
+        Serial.print(grnVal);
+        Serial.print(" / ");  
+        Serial.println(bluVal); 
+      } 
+      DEBUG += 1;
+    }
+  }
+  // Update current values for next loop
+  prevR = redVal; 
+  prevG = grnVal; 
+  prevB = bluVal;
+  delay(hold); // Pause for optional 'wait' milliseconds before resuming the loop
+}
