cb19896baa44787d669301897df26037c646dafc
Jacob Bates <jacob.bates@gmail.com>
2012-05-07 23:57:36 -0400

    Code cleanup.

--- Arduino Projects/SingleChannelRGBTimeControlled/SingleChannelRGBTimeControlled.ino
+++ Arduino Projects/SingleChannelRGBTimeControlled/SingleChannelRGBTimeControlled.ino
@@ -42,13 +42,6 @@ int redVal;
 int grnVal;
 int bluVal;
 
-/*
-// Initialize color variables
-int prevR = redVal;
-int prevG = grnVal;
-int prevB = bluVal;
-*/
-
 // Sunrise/sunset variables
 TimeLord timeLord;
 byte sunRise[6] = {0, 0, 0, 0, 0, 0};
@@ -62,17 +55,8 @@ uint32_t NOON = 43200;
 byte prevHour = 0;
 byte prevDayOfMonth = 0;
 
-/*
-int wait = 5;      // 10ms internal crossFade delay; increase for slower fades
-int hold = 5000;       // Optional hold when a color is complete, before the next crossFade
-*/
-
 boolean DEBUG = true;
 
-byte bcdToDec(byte val)  {
-// Convert binary coded decimal to normal decimal numbers
-  return ( (val/16*10) + (val%16) );
-}
 
 void setup() {
   Wire.begin();
@@ -251,96 +235,3 @@ void printSunriseSunset(){
   Serial.print(sunSet[0], DEC);
   Serial.println();
 }
-
-/* BELOW THIS LINE IS THE MATH -- YOU SHOULDN'T NEED TO CHANGE THIS FOR THE BASICS
-* 
-* The program works like this:
-* Imagine a crossfade that moves the red LED from 0-10, 
-*   the green from 0-5, and the blue from 10 to 7, in
-*   ten steps.
-*   We'd want to count the 10 steps and increase or 
-*   decrease color values in evenly stepped increments.
-*   Imagine a + indicates raising a value by 1, and a -
-*   equals lowering it. Our 10 step fade would look like:
-* 
-*   1 2 3 4 5 6 7 8 9 10
-* R + + + + + + + + + +
-* G   +   +   +   +   +
-* B     -     -     -
-* 
-* The red rises from 0 to 10 in ten steps, the green from 
-* 0-5 in 5 steps, and the blue falls from 10 to 7 in three steps.
-* 
-* In the real program, the color percentages are converted to 
-* 0-255 values, and there are 1020 steps (255*4).
-* 
-* To figure out how big a step there should be between one up- or
-* down-tick of one of the LED values, we call calculateStep(), 
-* which calculates the absolute gap between the start and end values, 
-* and then divides that gap by 1020 to determine the size of the step  
-* between adjustments in the value.
-
-
-int calculateStep(int prevValue, int endValue) {
-  int step = endValue - prevValue; // What's the overall gap?
-  if (step) {                      // If its non-zero, 
-    step = 1020/step;              //   divide by 1020
-  } 
-  return step;
-}
-
-/* The next function is calculateVal. When the loop value, i,
-*  reaches the step size appropriate for one of the
-*  colors, it increases or decreases the value of that color by 1. 
-*  (R, G, and B are each calculated separately.)
-
-
-int calculateVal(int step, int val, int i) {
-  if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
-    if (step > 0) {              //   increment the value if step is positive...
-      val += 1;           
-    } 
-    else if (step < 0) {         //   ...or decrement it if step is negative
-      val -= 1;
-    } 
-  }
-  // Defensive driving: make sure val stays in the range 0-255
-  if (val > 255) {
-    val = 255;
-  } 
-  else if (val < 0) {
-    val = 0;
-  }
-  return val;
-}
-
-/* crossFade() converts the percentage colors to a 
-*  0-255 range, then loops 1020 times, checking to see if  
-*  the value needs to be updated each time, then writing
-*  the color values to the correct pins.
-
-
-void crossFade(int color[3]) {
-  int stepR = calculateStep(prevR, color[0]);
-  int stepG = calculateStep(prevG, color[1]); 
-  int stepB = calculateStep(prevB, color[2]);
-
-  for (int i = 0; i <= 1020; i++) {
-    redVal = calculateVal(stepR, redVal, i);
-    grnVal = calculateVal(stepG, grnVal, i);
-    bluVal = calculateVal(stepB, bluVal, i);
-
-    analogWrite(redPin, redVal);   // Write current values to LED pins
-    analogWrite(grnPin, grnVal);      
-    analogWrite(bluPin, bluVal); 
-
-    delay(wait); // Pause for 'wait' milliseconds before resuming the loop
-  }
-  
-  // Update current values for next loop
-  prevR = redVal; 
-  prevG = grnVal; 
-  prevB = bluVal;
-  delay(hold); // Pause for optional 'wait' milliseconds before resuming the loop
-}
-*/
